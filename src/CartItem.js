10885e4b3831663e21ea8e3ed45089b151b86e70
13-ritika <ritikagarg134@gmail.com>
2021-10-22 23:58:20 +0530

    add props to cartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,90 +1,99 @@
 import React from 'react';
-{/* for a class com to be a react com, give method render which should return some jsx which describes the UI of com.*/}
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price: 99999,
-            title: 'Phone',
-            qty:1,
-            img: ''
-        }
-       // this.increaseQuantity =this.increaseQuantity.bind(this);
-       // use arrow functions, as they automatically bind the functions.
+
+class CartItem extends React.Component {
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
+
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     this.setState({ qty: this.state.qty + 10 });
+
+  //     console.log('state', this.state);
+  //   });
+  // }
+  increaseQuantity = () => {
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // }, () => {});
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
     }
-    increaseQuantity = () =>{
-        // this.state.qty += 1;
-        console.log('this', this.state);
-        // setState form 1 { by giving object}, react does shallow merging.
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // });
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty +1
-            }
-        });
-    }
-    decreaseQuantity=()=>{
-        // this.state.qty += 1;
-        console.log('this', this.state);
-        // setState form 1 { by giving object}, react does shallow merging.
-        // this.setState({
-        //     qty: this.state.qty -1 
-        // });
-        //form2
-        this.setState((prevState) =>{
-            return {
-                qty: prevState.qty -1
-            }
-        });
-        // if prevstate required, use form 2
-    }
-    render(){
-        const{price,title,qty} = this.state;
-        return(
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style = {styles.image}/>
-
-                </div>
-                <div className="right-block">
-                    <div style={ {fontSize:25}}>{title}</div>
-                    <div style={ {color:'#777'}}>Rs {price}</div>
-                    <div style={ {color:'#777'}}>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                        <img 
-                         alt="increase" 
-                         className="action-icons" 
-                         src="https://cdn-icons-png.flaticon.com/512/1828/1828926.png" 
-                         onClick={this.increaseQuantity}
-                        />
-                        <img
-                         alt="decrease "
-                         className="action-icons" 
-                         src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1634759487~hmac=8b31624e9c6b64d57ccf53e4c0bca498" 
-                         onClick={this.decreaseQuantity}
-                         />
-                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3096/3096673.png" />
-
-                    </div>
-
-                </div>
-
-            </div>
-
-        );
-    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
+    });
+  }
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
+    return (
+      <div className="cart-item">
+        {this.props.jsx}
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons.flaticon.com/png/512/2740/premium/2740679.png?token=exp=1634925883~hmac=bbc51602beddbe3ec86c6d87bc2b957d"
+              onClick={this.decreaseQuantity}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons.flaticon.com/png/512/484/premium/484662.png?token=exp=1634928019~hmac=40f761380685927f4a4064a6ea1a6855"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
 
 export default CartItem;
